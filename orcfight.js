7394baad03b15e9e2c935d85a9a8e83ea9f8b854
Matt Haxby <matthaxby@gmail.com>
2015-05-07 00:09:14 -0600

    don't ask me

---
+++ orcfight.js
@@ -0,0 +1,57 @@
+
+var orcA = {
+   name: "Walrus",
+   strength: 10,
+   specialMoveOne: "mustache-grinds",
+   superSpecialMove: "administers an ULTRA-INVASIVE TUSK PROCEDURE on",
+   onDeathPhrase: "",
+   health: 140
+};
+
+var orcB = {
+   name: "Mango",
+   strength: 15,
+   specialMoveOne: "sticky-punches",
+   superSpecialMove: "DANCES FURIOUSLY upon",
+   health: 100
+};
+
+
+
+var fight = function(orcA, orcB, healthA, healthB) {
+  var randomA = Math.random();
+  if (randomA < 0.2) {
+    healthA = healthA - orcB.strength*2;
+    console.log(orcB.name + " " + orcB.superSpecialMove + " " + orcA.name + "! Health at" + " " + healthA);
+  }
+  else if (randomA < 0.8) {
+    healthA = healthA - orcB.strength;
+    console.log(orcB.name + " " + orcB.specialMoveOne + " " + orcA.name + "! Health at" + " " + healthA);
+  }
+  else {
+    console.log(orcB.name + " missed!");
+  }
+    if(healthA <= 0) {
+    return orcA.name + " has been defeated.  " + orcB.name + ", FINISH HIM!";
+  }
+  var randomB = Math.random();
+  if (randomB < 0.2) {
+  healthB = healthB - orcA.strength*2;
+  console.log(orcA.name + " " + orcA.superSpecialMove + " " + orcB.name + "!" + " " + orcB.name + "'s" + " health is now at" + " " + healthB);
+  }
+  else if (randomB < 0.8){
+    healthB = healthB - orcA.strength;
+    console.log(orcA.name + " " + orcA.specialMoveOne + " " + orcB.name + "!  Health at" + " " + healthB);
+  }
+  else {
+        console.log(orcA.name + " missed!");
+  }
+  if (healthB <= 0) {
+    return orcB.name + " is crying, A LOT.";
+  }
+  else {
+    return fight(orcA, orcB, healthA, healthB);
+  }
+};
+
+console.log(fight(orcA, orcB, orcA.health, orcB.health));
